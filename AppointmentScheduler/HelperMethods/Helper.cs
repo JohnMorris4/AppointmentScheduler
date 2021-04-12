b9525084cef0e3ac3dc17596ae6524fffa9294a8
johnmorris4 <johnmorris4@bellsouth.net>
2021-04-12 08:09:37 -0400

    Added the Login and Register partials

--- AppointmentScheduler/HelperMethods/Helper.cs
+++ AppointmentScheduler/HelperMethods/Helper.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
 namespace AppointmentScheduler.HelperMethods
 {
     public static class Helper
@@ -5,5 +8,18 @@ namespace AppointmentScheduler.HelperMethods
         public static string Admin = "Admin";
         public static string Manager = "Manager";
         public static string Mechanic = "Mechanic";
+        public static string Customer = "Customer";
+
+
+        public static List<SelectListItem> GetRolesForDropDown()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem {Value = Helper.Admin, Text = Helper.Admin},
+                new SelectListItem {Value = Helper.Manager, Text = Helper.Manager},
+                new SelectListItem {Value = Helper.Mechanic, Text = Helper.Mechanic},
+                new SelectListItem {Value = Helper.Customer, Text = Helper.Customer}
+            };  
+        }
     }
 }
--- AppointmentScheduler/Startup.cs
+++ AppointmentScheduler/Startup.cs
@@ -6,6 +6,7 @@ using AppointmentScheduler.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -30,6 +31,7 @@ namespace AppointmentScheduler
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
             services.AddControllersWithViews();
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
